b8911d979c749009cd507873cfabe055b86e82b4
Danil Garmashov <wizzy_fox@mail.ru>
2023-05-08 23:07:14 +0700

    feat: task 4 done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
 
+const fs = require('fs');
+const path = require('path');
+const fsPromises = fs.promises;
+const copyFile = fsPromises.copyFile;
+
+(function copyDir() {
+    fs.mkdir(path.join(__dirname, 'files-copy'), {
+        recursive: true,
+    }, err => {
+        if (err) {
+            throw new Error('Folder already exists!')
+        }
+        console.log('Folder created!');
+    });
+
+    fsPromises.readdir(path.join(__dirname, 'files'))
+        .then(files => {
+            files.forEach(file => {
+                const filePath = path.join(__dirname, 'files', file);
+                const copyPath = path.join(__dirname, 'files-copy', file);
+                copyFile(filePath, copyPath);
+                console.log(file);
+            });
+        });
+})();
